9bbe207f3d5a9aabc11e524d262a1e06f3b74fd2
gabriiellrss <gabriel.santana166@etec.sp.gov.br>
2025-05-05 17:55:09 -0300

    Mapa

---
+++ Assets/MapaColi.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class MapaColi : MonoBehaviour
+{
+    void Start()
+    {
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+
+        foreach (MeshFilter mf in meshFilters)
+        {
+            GameObject go = mf.gameObject;
+
+            if (go.GetComponent<MeshCollider>() == null)
+            {
+                MeshCollider mc = go.AddComponent<MeshCollider>();
+                mc.convex = false;
+            }
+        }
+
+        Debug.Log("✅ Mesh Colliders adicionados a todos os objetos com malha.");
+    }
+}
