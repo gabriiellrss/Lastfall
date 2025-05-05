eb67f4b0a9ddd03a8be929ed3ab57db76ebcf6b9
Daviisq <davi.domingos@etec.sp.gov.br>
2025-05-05 16:58:36 -0300

    Menu

---
+++ Assets/Tetse/MenuPrincipalMenagemant.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+
+public class MenuPrincipalMenagemant : MonoBehaviour
+{
+    public void Jogar()
+    {
+        SceneManager.LoadScene("SampleScene")
+    }
+    public void Load()
+    {
+
+    }
+    public void Config()
+    {
+
+    }
+    public void Creditos()
+    {
+
+    }
+    public void Sair()
+    {
+
+    }
+}
